7bf932ed20521166ec285dff5e0816de6a9c77df
JeevanKr_Sahu <jeevank028@gmail.com>
2024-09-14 13:42:12 +0530

    14/09/2024

---
+++ CSS/07-09-2024/Practice.js
@@ -0,0 +1,6 @@
+// Declare your name as a string and print its length in js
+let nam = "Jeevan kumar Sahu"
+console.log("Lenth of name : ",nam.length)
+
+console.log(nam[0]);
+console.log(nam[nam.length-1]);
